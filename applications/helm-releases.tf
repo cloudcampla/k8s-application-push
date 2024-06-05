7d21fbc463ea770af62db88ff78d919673399ae6
Brayan Bautista <Braybaut@gmail.com>
2024-06-05 18:54:23 -0500

    new release

--- applications/helm-releases.tf
+++ applications/helm-releases.tf
@@ -2,5 +2,12 @@ resource "helm_release" "redis" {
   name       = "redis-cloudcamp"
   repository = "https://charts.bitnami.com/bitnami"
   chart      = "redis"
+  version    = "19.5.1"
+}
+
+resource "helm_release" "redis-2" {
+  name       = "redis-cloudcamp-2"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "redis"
   version    = "19.5.0"
 }
