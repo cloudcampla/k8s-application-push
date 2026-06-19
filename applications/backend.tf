80b37f7fe3ddad4a1d8d4fd76a465b802c2e6906
Brayan Bautista <braybaut@gmail.com>
2026-06-18 19:53:15 -0500

    fix: changes on workflows

--- applications/backend.tf
+++ applications/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "project03-terraform-state12345"
+    bucket = "cafeluna-cfn-templates-948285518339-us-east-1"
     key    = "k8s-application-push/state.tfstate"
     region = "us-east-1"
   }
